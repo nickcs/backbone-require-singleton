d90fac63ce7eb8ecb91f0b9bef91027542c6b4d3
Nick Nance <nance.nick@gmail.com>
2015-01-23 10:05:45 -0600

    big fix in app to wait for all resources to load before running app code

--- app/scripts/app/app.js
+++ app/scripts/app/app.js
@@ -1,27 +1,32 @@
-'use strict';
 define([
+    'jquery',
     'backbone',
     'broker',
     './views/nav',
     './views/footer'
-], function (Backbone, broker, navView, FooterView) {
+], function ($, Backbone, broker, navView, FooterView) {
+    'use strict';
 
     Backbone.View.prototype.attachToTemplate = true;
 
-    // create application container view
-    var container = new Backbone.View({el: '.container'});
-    // create body view that modules will use for display
-    var body = new Backbone.View();
+    // this is required to make sure everything is loaded before
+    // the application starts running
+    $( document ).ready( function() {
+        // create application container view
+        var container = new Backbone.View({el: '.container'});
+        // create body view that modules will use for display
+        var body = new Backbone.View();
 
-    // build structure of the container view
-    container.addSubView({view: navView});
-    container.addSubView({view: body});
-    container.addSubView({view: new FooterView()});
+        // build structure of the container view
+        container.addSubView({view: navView});
+        container.addSubView({view: body});
+        container.addSubView({view: new FooterView()});
 
-    broker.channel('container').subscribe('show', function(view){
-        body.setView(view);
+        broker.channel('container').subscribe('show', function(view){
+            body.setView(view);
+        });
+
+        broker.start();
+        Backbone.history.start();
     });
-
-    broker.start();
-    Backbone.history.start();
 });
